c3843d410260a093380cf6e0a4445f192a9199d1
teoboley <teo751@gmail.com>
2019-02-17 18:54:27 -0500

    Modified Tree component.

--- src/components/molecules/Tree/index.tsx
+++ src/components/molecules/Tree/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { animated, useSpring } from 'react-spring';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 
 import MinimizeIcon from '@material-ui/icons/Remove';
 import MaximizeIcon from '@material-ui/icons/Add';
@@ -22,7 +22,7 @@ interface ITreeViewModel {
 type TreeProps = ITreeViewModel;
 
 export const Tree: React.NamedExoticComponent<TreeProps> = React.memo(
-  ({ children, name, style, open = false }) => {
+  ({ children, name, style, open = false, className }) => {
     const [isOpen, setOpen] = useState(open);
     const previous = usePrevious(isOpen);
     const [bind, { height: viewHeight }] = useMeasure<HTMLDivElement>();
@@ -41,35 +41,30 @@ export const Tree: React.NamedExoticComponent<TreeProps> = React.memo(
 
     return (
       <div
-        className={css({
-          position: 'relative',
+        className={cx(css({
           padding: '4px 0px 0px 0px',
           textOverflow: 'ellipsis',
           whiteSpace: 'nowrap',
-          overflowX: 'hidden',
-          verticalAlign: 'middle',
-          color: 'white',
-          fill: 'white'
-        })}
+          verticalAlign: 'middle'
+        }), className)}
+        style={style}
       >
-        <ButtonBase type={EButtonType.Highlight} className={css({ position: 'relative', padding: 0, top: -2, marginRight: 10 })} disabled={!children}><Icon
+        <ButtonBase type={EButtonType.Highlight} className={css({ position: 'relative', padding: 0, top: -2, marginRight: 10 })} disabled={!children} onClick={() => void setOpen(!isOpen)}><Icon
           style={{
             width: '1em',
             height: '1em',
             cursor: 'pointer',
             verticalAlign: 'middle'
           }}
-          onClick={() => void setOpen(!isOpen)}
         /></ButtonBase>
         <Text
           type={ETextType.Label}
-          style={{ display: 'inline-block', verticalAlign: 'middle', ...style }}
+          style={{ display: 'inline-block', verticalAlign: 'middle' }}
         >
           {name}
         </Text>
         <animated.div
           className={css({
-            position: 'relative',
             willChange: 'transform, opacity, height',
             marginLeft: '6px',
             padding: '0px 0px 0px 14px',
