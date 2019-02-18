7529e1aea4ebb30d9c0c829a33bb785299638d57
teoboley <teo751@gmail.com>
2019-02-17 19:13:12 -0500

    Simplified Snackbar story.

--- src/components/atoms/Snackbar/index.stories.tsx
+++ src/components/atoms/Snackbar/index.stories.tsx
@@ -1,55 +1,8 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import { storiesOf } from '@storybook/react';
 
-import { MessageHub, Snackbar, AddMessageFunc, MessageProvider, useMessage } from '.';
+import { Snackbar, MessageProvider, useMessage } from '.';
 import Button, { EButtonType } from '../Button';
-import { useEffect, useRef, useState } from 'react';
-
-// const messagesRoot = document.createElement("div");
-// messagesRoot.id = 'messages-root';
-// document.body.appendChild(messagesRoot);
-//
-// const useMessage = () => {
-//   const [state, setState] = useState<AddMessageFunc | null>(null);
-//
-//   return { showMessage: state, setRef: (newRef: AddMessageFunc) => {
-//       setState(newRef);
-//   }};
-// };
-//
-// export const Test: React.FC<{}> = () => {
-//   const { showMessage, setRef } = useMessage();
-//
-//   return (
-//     <div>
-//       <Snackbar heading={'Snackbar Heading'} description={'Some description'}>
-//         <Button type={EButtonType.Outline}>Bye</Button>
-//         <Button type={EButtonType.Contained}>Aight</Button>
-//       </Snackbar>
-//       <Button
-//         onClick={() =>
-//           showMessage && showMessage(
-//             onClose => (
-//               <Snackbar
-//                 heading={'Snackbar Heading'}
-//                 description={'Some description'}
-//                 dismissable
-//                 onClose={onClose}
-//               >
-//                 <Button type={EButtonType.Outline} onClick={onClose}>Bye</Button>
-//                 <Button type={EButtonType.Contained}>Aight</Button>
-//               </Snackbar>
-//             )
-//           )
-//         }
-//       >
-//         Add Snackbar
-//       </Button>
-//       <MessageHub children={add => setRef(add)} />
-//     </div>
-//   );
-// };
 
 export const Test: React.FC<{}> = () => {
   const showMessage = useMessage();
